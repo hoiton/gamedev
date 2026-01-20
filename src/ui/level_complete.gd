fb35e11d9290ddd03af5eab3ac2ec894a24ecffd
hoiton <timscheiber87@gmail.com>
2026-01-20 20:51:06 +0100

    Added Level-Progression

--- src/ui/level_complete.gd
+++ src/ui/level_complete.gd
@@ -3,5 +3,10 @@ extends Control
 func _ready() -> void:
 	self.theme = UITheme.build()
 
+
+func _on_next_level_pressed() -> void:
+	GameManager.next_level()
+
+
 func _on_menu_pressed() -> void:
 	GameManager.back_to_menu()
