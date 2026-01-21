c59d5e675c2ec863a6343ddac9f9830284cebc0a
hoiton <timscheiber87@gmail.com>
2026-01-21 20:09:34 +0100

    End Voice Line

--- src/ui/victory.gd
+++ src/ui/victory.gd
@@ -1,6 +1,9 @@
 extends Control
 
+@onready var good_job: AudioStreamPlayer2D = $GoodJob
+
 func _ready() -> void:
+	good_job.play()
 	self.theme = UITheme.build()
 
 func _on_menu_pressed() -> void:
